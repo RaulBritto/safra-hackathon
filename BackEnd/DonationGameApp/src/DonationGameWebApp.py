9de15505da27c4df2b2bf27908a5acab19442e37
Gustavo de Almeida <gsutavo@outlook.com>
2020-09-12 18:36:00 -0300

    Classe que interage com o DB

--- BackEnd/DonationGameApp/src/DonationGameWebApp.py
+++ BackEnd/DonationGameApp/src/DonationGameWebApp.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 from Authentication import AuthenticationHandler
 from SafraAPI import SafraAPI
+from DB_Interact import DB_interact
+from Models import Schema
 
 import logging
 import sys, os
 import json
-
+Schema('../market_v2.db')
 logging.basicConfig(level=logging.DEBUG)
-
+db_interactions = DB_interact()
 app = Flask(__name__)
-
-
 
 @app.after_request
 def add_headers(response):
@@ -37,6 +37,15 @@ def getStore(storeId):
 def payment(userId):
     return "Pagamento " + userId
 
+@app.route('/Add/<accountId>/<storeId>/<productId>/<quantity>')
+def add_cart(accountId,storeId, productId, quantity):
+    db_interactions.add_to_cart(accountId,storeId, productId, quantity)
+    return db_interactions.return_cart_total()
+
 @app.route("/about")
 def About():
     return "About us"
+    
+if __name__ == "__main__":
+    #Schema(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','presencecontrol.db'))
+    app.run(debug=True)
