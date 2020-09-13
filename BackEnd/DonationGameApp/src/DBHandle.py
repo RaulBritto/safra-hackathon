34bfad16152d44442ff92e313834e3f1ba061eb7
Raul Britto <raulbbritto@gmail.com>
2020-09-13 11:18:17 -0300

    feat: new query to stores

--- BackEnd/DonationGameApp/src/DBHandle.py
+++ BackEnd/DonationGameApp/src/DBHandle.py
@@ -6,10 +6,12 @@ class DBHandle():
     
     def GetProducts(self, storeID, productID= None):
         if not productID: 
-           return [i.product_id for i in ProductDB.select()]
+           return [i for i in ProductDB.select().where(ProductDB.store == storeID)]
         else:
             if type(productID) is not list:
                 return ProductDB.select().where(ProductDB.product_id == productID, ProductDB.store == storeID).get()
             else:
                 return [ProductDB.select().where(ProductDB.product_id == _id, ProductDB.store == storeID).get() for _id in productID]
 
+    def GetStore(self, storeID):
+        return StoreDB.select().where(StoreDB.store_id == storeID).get()
