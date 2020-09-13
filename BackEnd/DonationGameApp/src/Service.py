7b4348ed49e5fc7ccd2b3e4fdc20938f2f17fcb5
Raul Britto <raulbbritto@gmail.com>
2020-09-13 14:19:05 -0300

    feat: new funconalities to Service class

--- BackEnd/DonationGameApp/src/Service.py
+++ BackEnd/DonationGameApp/src/Service.py
@@ -1,10 +1,23 @@
 from Authentication import *
+from ModelsDB import Schema
+from Models import *
+from DBHandle import DBHandle
 
-class DonationGameService:
+db_interactions = DBHandle()
+
+class SafraMarketService:
     @staticmethod
     def login(accountId):
-        
         AuthenticationHandler.GetAuthenticationToken()
+    
+    @staticmethod
+    def getProductsFromStore(storeId):
+        return db_interactions.GetProducts(storeId)
+        
+    @staticmethod
+    def getStoreInfo(storeId):
+        return db_interactions.GetStore(storeId)
+
 
 
 
